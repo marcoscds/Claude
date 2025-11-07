86a748be15aadd87b9cb33873f739ea64943611b
marcoscds <113212276+marcoscds@users.noreply.github.com>
2025-11-07 06:42:04 -0300

    Update __init__.py

--- recipes/matplotlib/__init__.py
+++ recipes/matplotlib/__init__.py
@@ -1,1 +1,23 @@
+import os
+from pythonforandroid.recipes.matplotlib import MatplotlibRecipe
 
+class FixedMatplotlibRecipe(MatplotlibRecipe):
+    need_stl_shared = True
+    patches = []
+
+    def get_recipe_env(self, arch=None, **kwargs):
+        env = super().get_recipe_env(arch, **kwargs)
+        env["MPLBACKEND"] = "Agg"
+        env["USE_OPENMP"] = "0"
+        
+        if "CFLAGS" not in env:
+            env["CFLAGS"] = ""
+        if "CXXFLAGS" not in env:
+            env["CXXFLAGS"] = ""
+            
+        env["CFLAGS"] += " -Wno-error"
+        env["CXXFLAGS"] += " -Wno-error -std=c++17"
+        
+        return env
+
+recipe = FixedMatplotlibRecipe()
